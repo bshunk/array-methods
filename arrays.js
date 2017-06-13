6550b9d7f55643670253363ce1bc21015196cf5e
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-13 14:05:46 -0500

    finish array methods

--- arrays.js
+++ arrays.js
@@ -33,8 +33,10 @@ let sum = numbers.reduce( function(prev, curr) {
 console.log("sum", sum);
 
 
+
 // exercise
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+console.log("beginning values for the numbers: ", integers);
 
 // Sort the numbers in descending order (10, 9, 8, 7, etc).
 // Remove any integers greater than 19.
